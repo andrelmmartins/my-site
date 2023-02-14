179bd4106670dab6c48c8456d8ba3ce3f1643fd5
André Martins <andre.lucas.medeir@gmail.com>
2023-02-14 18:56:32 -0300

    styles: chackra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles'
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
+        <ChakraProvider theme={theme}>
         <Component {...pageProps} />
+        </ChakraProvider>
     )
 }
